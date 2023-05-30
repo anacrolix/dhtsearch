93fa64e96f59691e0858571b56c83eb7e16a0ce3
Matt Joiner <anacrolix@gmail.com>
2023-05-30 13:06:27 +1000

    Switch to uncontrolled query input

--- src/leptos.rs
+++ src/leptos.rs
@@ -1,19 +1,26 @@
 use crate::api::*;
+use leptos::html::Input;
 use leptos::*;
+use web_sys::SubmitEvent;
 
 #[component]
 fn App(cx: Scope) -> impl IntoView {
     let (query, set_query) = create_signal(cx, None);
+    let query_input: NodeRef<Input> = create_node_ref(cx);
     let torrents = create_local_resource(
         cx,
         query,
         |query| async move { search(query.unwrap()).await },
     );
+    let on_query_submit = move |ev: SubmitEvent| {
+        ev.prevent_default();
+        set_query(query_input().map(|input| input.value()));
+    };
     view! { cx,
         <h1>{ "DHT search" }</h1>
-        <input type="text" on:input=move |ev| {
-            set_query(Some(event_target_value(&ev)));
-        }/>
+        <form on:submit=on_query_submit>
+            <input type="text" node_ref=query_input/>
+        </form>
         <div>
             <h3>{"Torrents"}</h3>
             <TorrentsListLeptos search={Signal::derive(cx, move || torrents.read(cx))}/>
@@ -29,7 +36,8 @@ fn TorrentsListLeptos(cx: Scope, search: Signal<Option<InfosSearch>>) -> impl In
                 search
                     .items
                     .into_iter()
-                    .map(|torrent| view! { cx, <tr>{torrent.name}</tr>}).collect_view(cx)
+                    .map(|torrent| view! { cx, <tr>{torrent.name}</tr>})
+                    .collect_view(cx)
             })
             .unwrap_or_default()
     };
